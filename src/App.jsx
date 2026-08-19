2801b735ca92587d6c2a7241ab04c30a15d8fc1a
Pranav Sharma <sharmapranav98766@gmail.com>
2026-08-19 22:53:23 +0530

    Achivement:Navbar is Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-     {/* <Navbar />
+     <Navbar />
      <Hero />
-     <Skills /> */}
+     <Skills />
     </>
   )
 }
