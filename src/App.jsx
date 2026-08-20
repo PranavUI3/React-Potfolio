5985abafa226659dc82a462f09803a5b5e54e820
Pranav Sharma <sharmapranav98766@gmail.com>
2026-08-20 18:52:01 +0530

    Achivement:About ME section is now added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Skills from './components/Skills'
+import Aboutme from './components/Aboutme'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
      <Navbar />
      <Hero />
+     <Aboutme />
      <Skills />
     </>
   )
